d37a48be8678c1a963912c83708b64a3699afe8d
GallonEmilien <gallonemilien@gmail.com>
2025-10-02 21:55:46 +0200

    chore: 1.20.6 port

---
+++ neoforge/src/main/java/fr/gallonemilien/ridetogether/neoforge/RideTogetherNeoForge.java
@@ -0,0 +1,11 @@
+package fr.gallonemilien.ridetogether.neoforge;
+
+import fr.gallonemilien.ridetogether.RideTogether;
+import net.neoforged.fml.common.Mod;
+
+@Mod(RideTogether.MOD_ID)
+public final class RideTogetherNeoForge {
+    public RideTogetherNeoForge() {
+        RideTogether.init();
+    }
+}
